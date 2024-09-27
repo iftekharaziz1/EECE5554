45487e42fea7be1c1729585f91977bfe58ba47ae
iftekharaziz1 <aziz.i@northeastern.edu>
2024-09-26 22:24:49 -0400

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iftekhar/catkin_ws/src/EECE5554/lab0
-# Build directory: /home/iftekhar/catkin_ws/src/EECE5554/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
